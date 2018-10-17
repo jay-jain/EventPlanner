a761e1a1391822a267e2cbada880a77eefef1e44
Jay Jain <jjain654@gmail.com>
2018-10-17 15:56:57 -0500

    Add Task.cs class

---
+++ EventPlanner/Task.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventPlanner
+{
+    public class Task:IComparable<Task>
+    {
+
+        private string title;
+        private DateTime time;
+        private string notes;
+
+        public Task(String title, DateTime time, String notes)
+        {
+            this.title = title;
+            this.time = time;
+            this.notes = notes;
+
+        }
+
+        public void setTitle(String title)
+        {
+            this.title = title;
+        }
+
+        public void setTime(DateTime time)
+        {
+            this.time = time;
+        }
+
+        public void setNotes(String notes)
+        {
+            this.notes = notes;
+        }
+
+        public string getTitle()
+        {
+            return this.title;
+        }
+
+        public DateTime getTime()
+        {
+            return this.time;
+        }
+
+        public string getNotes()
+        {
+            return this.notes;
+        }
+
+        public int CompareTo(Task next)
+        {
+            return time.CompareTo(next.time);
+        }
+    }
+}
